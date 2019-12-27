1c916b0cd486e404e053047d3821c0acc0b1a858
kvdkiran <57666006+kvdkiran@users.noreply.github.com>
2019-12-27 14:44:29 +0530

    Add files via upload

---
+++ jj.java
@@ -0,0 +1,11 @@
+import java.io.*;
+public class jj {
+
+   public static void main(String[] args) {
+        
+      int num1 = 5, num2 = 15, sum;
+      sum = num1 + num2;
+
+      System.out.println("Sum of these numbers: "+sum);
+   }
+}
